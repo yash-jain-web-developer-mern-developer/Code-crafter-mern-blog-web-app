22d54346fd9a2ebb07361cd08f7a9e7df427ced1
Yash jain <yjain2529@gmail.com>
2024-10-12 00:08:14 +0530

    change grid size

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,11 +30,11 @@ export default function Home() {
       </div>
      
 
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+      <div className='max-w-8xl mx-auto p-3 flex flex-col gap-8 py-6'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
             <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
-            <div className='flex flex-wrap gap-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
